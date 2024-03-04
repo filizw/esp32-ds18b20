df51d58e2945452f4c7af2d3cc29efbb415b84f4
filizw <fizworski202@gmail.com>
2024-03-04 22:01:05 +0100

    Add basic error handling to driver

--- src/ds18b20.c
+++ src/ds18b20.c
@@ -7,213 +7,234 @@ static const uint32_t LEVEL_LOW = 0;
 
 //static const uint32_t conversion_time_us[] = {93750, 187500, 375000, 750000};
 
-static void ds18b20_write_0(const ds18b20_handle_t *const handle) {
-    if(handle != NULL) {
-        gpio_set_direction(handle->gpio_pin, GPIO_MODE_OUTPUT);
-        gpio_set_level(handle->gpio_pin, LEVEL_LOW);
-        ets_delay_us(60);
-        gpio_set_direction(handle->gpio_pin, GPIO_MODE_INPUT);
-        ets_delay_us(2);
-    }
-    else {
-        ESP_LOGE(TAG, "handle is NULL");
-    }
+static ds18b20_error_t ds18b20_write_0(const ds18b20_handle_t *const handle) {
+    if(handle == NULL)
+        return DS18B20_ERROR_INVALID_HANDLE;
+
+    gpio_set_direction(handle->gpio_pin, GPIO_MODE_OUTPUT);
+    gpio_set_level(handle->gpio_pin, LEVEL_LOW);
+    ets_delay_us(60);
+    gpio_set_direction(handle->gpio_pin, GPIO_MODE_INPUT);
+    ets_delay_us(2);
+
+    return DS18B20_OK;
 }
 
-static void ds18b20_write_1(const ds18b20_handle_t *const handle) {
-    if(handle != NULL) {
-        gpio_set_direction(handle->gpio_pin, GPIO_MODE_OUTPUT);
-        gpio_set_level(handle->gpio_pin, LEVEL_LOW);
-        ets_delay_us(2);
-        gpio_set_direction(handle->gpio_pin, GPIO_MODE_INPUT);
-        ets_delay_us(60);
-    }
-    else {
-        ESP_LOGE(TAG, "handle is NULL");
-    }
+static ds18b20_error_t ds18b20_write_1(const ds18b20_handle_t *const handle) {
+    if(handle == NULL)
+        return DS18B20_ERROR_INVALID_HANDLE;
+
+    gpio_set_direction(handle->gpio_pin, GPIO_MODE_OUTPUT);
+    gpio_set_level(handle->gpio_pin, LEVEL_LOW);
+    ets_delay_us(2);
+    gpio_set_direction(handle->gpio_pin, GPIO_MODE_INPUT);
+    ets_delay_us(60);
+
+    return DS18B20_OK;
 }
 
-static void ds18b20_read_bit(const ds18b20_handle_t *const handle, uint8_t *const bit) {
-    if(handle != NULL && bit != NULL) {
-        gpio_set_direction(handle->gpio_pin, GPIO_MODE_OUTPUT);
-        gpio_set_level(handle->gpio_pin, LEVEL_LOW);
-        ets_delay_us(2);
-        gpio_set_direction(handle->gpio_pin, GPIO_MODE_INPUT);
-        ets_delay_us(8);
-        *bit = gpio_get_level(handle->gpio_pin);
-        ets_delay_us(52);
-    }
-    else {
-        ESP_LOGE(TAG, "handle or bit is NULL");
-    }
+static ds18b20_error_t ds18b20_read_bit(const ds18b20_handle_t *const handle, uint8_t *const bit) {
+    if(handle == NULL)
+        return DS18B20_ERROR_INVALID_HANDLE;
+    
+    if(bit == NULL)
+        return DS18B20_ERROR_INVALID_ARGUMENT;
+
+    gpio_set_direction(handle->gpio_pin, GPIO_MODE_OUTPUT);
+    gpio_set_level(handle->gpio_pin, LEVEL_LOW);
+    ets_delay_us(2);
+    gpio_set_direction(handle->gpio_pin, GPIO_MODE_INPUT);
+    ets_delay_us(8);
+    *bit = gpio_get_level(handle->gpio_pin);
+    ets_delay_us(52);
+
+    return DS18B20_OK;
 }
 
-static void ds18b20_write_byte(const ds18b20_handle_t *const handle, const uint8_t byte) {
-    if(handle != NULL) {
-        for(uint8_t i = 0; i < 8; i++) {
-            if(((byte >> i) & 1) == 0)
-                ds18b20_write_0(handle);
-            else
-                ds18b20_write_1(handle);
-        }
+static ds18b20_error_t ds18b20_write_byte(const ds18b20_handle_t *const handle, const uint8_t byte) {
+    if(handle == NULL)
+        return DS18B20_ERROR_INVALID_HANDLE;
+
+    for(uint8_t i = 0; i < 8; i++) {
+        if(((byte >> i) & 1) == 0)
+            ds18b20_write_0(handle);
+        else
+            ds18b20_write_1(handle);
     }
-    else {
-        ESP_LOGE(TAG, "handle or byte is NULL");
-    }
+
+    return DS18B20_OK;
 }
 
-static void ds18b20_read_byte(const ds18b20_handle_t *const handle, uint8_t *const byte) {
-    if(handle != NULL) {
-        *byte = 0;
-        uint8_t bit = 0;
+static ds18b20_error_t ds18b20_read_byte(const ds18b20_handle_t *const handle, uint8_t *const byte) {
+    if(handle == NULL)
+        return DS18B20_ERROR_INVALID_HANDLE;
+    
+    if(byte == NULL)
+        return DS18B20_ERROR_INVALID_ARGUMENT;
 
-        for(uint8_t i = 0; i < 8; i++) {
-            ds18b20_read_bit(handle, &bit);
-            *byte |= (bit << i);
-        }
+    *byte = 0;
+    uint8_t bit = 0;
+
+    for(uint8_t i = 0; i < 8; i++) {
+        ds18b20_read_bit(handle, &bit);
+        *byte |= (bit << i);
     }
-    else {
-        ESP_LOGE(TAG, "handle or byte is NULL");
-    }
+
+    return DS18B20_OK;
 }
 
-void ds18b20_reset(const ds18b20_handle_t *const handle) {
-    if(handle != NULL) {
-        gpio_set_direction(handle->gpio_pin, GPIO_MODE_OUTPUT);
-        gpio_set_level(handle->gpio_pin, LEVEL_LOW);
-        ets_delay_us(480);
-        gpio_set_direction(handle->gpio_pin, GPIO_MODE_INPUT);
-        ets_delay_us(70);
-        const int presence_level = gpio_get_level(handle->gpio_pin);
-        ets_delay_us(410);
-        const int recovery_level = gpio_get_level(handle->gpio_pin);
+ds18b20_error_t ds18b20_reset(const ds18b20_handle_t *const handle) {
+    if(handle == NULL)
+        return DS18B20_ERROR_INVALID_HANDLE;
 
-        ESP_LOGD(TAG, "reset: presence level: %d, recovery level: %d", presence_level, recovery_level);
-    }
-    else {
-        ESP_LOGE(TAG, "handle is NULL");
-    }
+    gpio_set_direction(handle->gpio_pin, GPIO_MODE_OUTPUT);
+    gpio_set_level(handle->gpio_pin, LEVEL_LOW);
+    ets_delay_us(480);
+    gpio_set_direction(handle->gpio_pin, GPIO_MODE_INPUT);
+    ets_delay_us(70);
+    const int presence_level = gpio_get_level(handle->gpio_pin);
+    ets_delay_us(410);
+    const int recovery_level = gpio_get_level(handle->gpio_pin);
+
+    ESP_LOGD(TAG, "reset: presence level: %d, recovery level: %d", presence_level, recovery_level);
+
+    return DS18B20_OK;
 }
 
-void ds18b20_send_command(const ds18b20_handle_t *const handle, const uint8_t command) {
+ds18b20_error_t ds18b20_send_command(const ds18b20_handle_t *const handle, const uint8_t command) {
     ds18b20_write_byte(handle, command);
+
+    return DS18B20_OK;
 }
 
-void ds18b20_read_rom(ds18b20_handle_t *const handle) {
-    if(handle != NULL) {
+ds18b20_error_t ds18b20_read_rom(ds18b20_handle_t *const handle) {
+    if(handle == NULL)
+        return DS18B20_ERROR_INVALID_HANDLE;
+
+    ds18b20_reset(handle);
+    ds18b20_send_command(handle, DS18B20_COMMAND_READ_ROM);
+
+    for(uint8_t i = 0; i < DS18B20_ROM_CODE_SIZE; i++) {
+        ds18b20_read_byte(handle, &handle->rom_code[i]);
+    }
+
+    return DS18B20_OK;
+}
+
+ds18b20_error_t ds18b20_write_scratchpad(const ds18b20_handle_t *const handle, const uint8_t *const data) {
+    if(handle == NULL)
+        return DS18B20_ERROR_INVALID_HANDLE;
+    
+    if(data == NULL)
+        return DS18B20_ERROR_INVALID_ARGUMENT;
+
+    ds18b20_reset(handle);
+    ds18b20_send_command(handle, DS18B20_COMMAND_SKIP_ROM);
+    ds18b20_send_command(handle, DS18B20_COMMAND_WRITE_SCRATCHPAD);
+
+    ds18b20_write_byte(handle, data[0]);
+    ds18b20_write_byte(handle, data[1]);
+    ds18b20_write_byte(handle, data[2]);
+
+    return DS18B20_OK;
+}
+
+ds18b20_error_t ds18b20_read_scratchpad(ds18b20_handle_t *const handle, const uint8_t num_of_bytes_to_read) {
+    if(handle == NULL)
+        return DS18B20_ERROR_INVALID_HANDLE;
+
+    ds18b20_reset(handle);
+    ds18b20_send_command(handle, DS18B20_COMMAND_SKIP_ROM);
+    ds18b20_send_command(handle, DS18B20_COMMAND_READ_SCRATCHPAD);
+
+    for(uint8_t i = 0; i < num_of_bytes_to_read && i < DS18B20_SCRATCHPAD_SIZE; i++) {
+        ds18b20_read_byte(handle, &handle->scratchpad[i]);
+    }
+
+    if(num_of_bytes_to_read < DS18B20_SCRATCHPAD_SIZE)
         ds18b20_reset(handle);
-        ds18b20_send_command(handle, DS18B20_COMMAND_READ_ROM);
-
-        for(uint8_t i = 0; i < DS18B20_ROM_CODE_SIZE; i++) {
-            ds18b20_read_byte(handle, &handle->rom_code[i]);
-        }
-    }
-    else {
-        ESP_LOGE(TAG, "handle is NULL");  
-    }
+    
+    return DS18B20_OK;
 }
 
-void ds18b20_write_scratchpad(const ds18b20_handle_t *const handle, const uint8_t *const data) {
-    if(handle != NULL && data != NULL) {
-        ds18b20_reset(handle);
-        ds18b20_send_command(handle, DS18B20_COMMAND_SKIP_ROM);
-        ds18b20_send_command(handle, DS18B20_COMMAND_WRITE_SCRATCHPAD);
+ds18b20_error_t ds18b20_init(ds18b20_handle_t *const handle, const ds18b20_config_t *const config) {
+    if(handle == NULL)
+        return DS18B20_ERROR_INVALID_HANDLE;
+    
+    if(config == NULL)
+        return DS18B20_ERROR_INVALID_CONFIG;
 
-        ds18b20_write_byte(handle, data[0]);
-        ds18b20_write_byte(handle, data[1]);
-        ds18b20_write_byte(handle, data[2]);
-    }
-    else {
-        ESP_LOGE(TAG, "handle or data is NULL");
-    }
+    ds18b20_configure(handle, config);
+
+    ds18b20_read_rom(handle);
+    ds18b20_read_scratchpad(handle, DS18B20_SCRATCHPAD_SIZE);
+
+    return DS18B20_OK;
 }
 
-void ds18b20_read_scratchpad(ds18b20_handle_t *const handle, const uint8_t num_of_bytes_to_read) {
-    if(handle != NULL) {
-        ds18b20_reset(handle);
-        ds18b20_send_command(handle, DS18B20_COMMAND_SKIP_ROM);
-        ds18b20_send_command(handle, DS18B20_COMMAND_READ_SCRATCHPAD);
+ds18b20_error_t ds18b20_configure(ds18b20_handle_t *const handle, const ds18b20_config_t *const config) {
+    if(handle == NULL)
+        return DS18B20_ERROR_INVALID_HANDLE;
+    
+    if(config == NULL)
+        return DS18B20_ERROR_INVALID_CONFIG;
 
-        for(uint8_t i = 0; i < num_of_bytes_to_read && i < DS18B20_SCRATCHPAD_SIZE; i++) {
-            ds18b20_read_byte(handle, &handle->scratchpad[i]);
-        }
+    const uint8_t data[] = {
+        config->trigger_high,
+        config->trigger_low,
+        config->resolution
+    };
 
-        if(num_of_bytes_to_read < DS18B20_SCRATCHPAD_SIZE)
-            ds18b20_reset(handle);
-    }
-    else {
-        ESP_LOGE(TAG, "handle is NULL");
-    }
+    ds18b20_write_scratchpad(handle, data);
+
+    ds18b20_read_scratchpad(handle, DS18B20_SCRATCHPAD_SIZE);
+
+    return DS18B20_OK;
 }
 
-void ds18b20_init(ds18b20_handle_t *const handle, const ds18b20_config_t *const config) {
-    if(handle != NULL) {
-        if(config != NULL) {
-            ds18b20_configure(handle, config);
-        }
+ds18b20_error_t ds18b20_read_temperature(ds18b20_handle_t *const handle, float *const temperature) {
+    if(handle == NULL)
+        return DS18B20_ERROR_INVALID_HANDLE;
 
-        ds18b20_read_rom(handle);
-        ds18b20_read_scratchpad(handle, DS18B20_SCRATCHPAD_SIZE);
-    }
-    else {
-        ESP_LOGE(TAG, "handle or config is NULL"); 
-    }
-}
+    ds18b20_reset(handle);
+    ds18b20_send_command(handle, DS18B20_COMMAND_SKIP_ROM);
+    ds18b20_send_command(handle, DS18B20_COMMAND_CONVERT);
 
-void ds18b20_configure(ds18b20_handle_t *const handle, const ds18b20_config_t *const config) {
-    if(handle != NULL && config != NULL) {
-        const uint8_t data[] = {
-            config->trigger_high,
-            config->trigger_low,
-            config->resolution
-        };
+    ets_delay_us(DS18B20_MAX_CONVERSION_TIME >> (3 - (handle->scratchpad[4] >> 5)));
 
-        ds18b20_write_scratchpad(handle, data);
+    ds18b20_read_scratchpad(handle, 2);
 
-        ds18b20_read_scratchpad(handle, DS18B20_SCRATCHPAD_SIZE);
-    }
-    else {
-        ESP_LOGE(TAG, "handle or config is NULL");
-    }
-}
-
-void ds18b20_read_temperature(ds18b20_handle_t *const handle, float *const temperature) {
-    if(handle != NULL) {
-        ds18b20_reset(handle);
-        ds18b20_send_command(handle, DS18B20_COMMAND_SKIP_ROM);
-        ds18b20_send_command(handle, DS18B20_COMMAND_CONVERT);
-
-        ets_delay_us(DS18B20_MAX_CONVERSION_TIME >> (3 - (handle->scratchpad[4] >> 5)));
-
-        ds18b20_read_scratchpad(handle, 2);
-
-        if(temperature != NULL) {
-            const uint8_t mask = 0xff << (3 - (handle->scratchpad[4] >> 5));
-            *temperature = ((handle->scratchpad[1] << 8) | (handle->scratchpad[0] & mask)) / 16.0f;
-        }
-    }
-    else {
-        ESP_LOGE(TAG, "handle is NULL");
-    }
-}
-
-void ds18b20_get_temperature(const ds18b20_handle_t *const handle, float *const temperature) {
-    if(handle != NULL && temperature != NULL) {
+    if(temperature != NULL) {
         const uint8_t mask = 0xff << (3 - (handle->scratchpad[4] >> 5));
         *temperature = ((handle->scratchpad[1] << 8) | (handle->scratchpad[0] & mask)) / 16.0f;
     }
-    else {
-        ESP_LOGE(TAG, "handle or temperature is NULL");
-    }
+
+    return DS18B20_OK;
 }
 
-void ds18b20_get_configuration(const ds18b20_handle_t *const handle, ds18b20_config_t *const config) {
-    if(handle != NULL && config != NULL) {
-        config->trigger_high = handle->scratchpad[2];
-        config->trigger_low = handle->scratchpad[3];
-        config->resolution = handle->scratchpad[4];
-    }
-    else {
-        ESP_LOGE(TAG, "handle or config is NULL");
-    }
+ds18b20_error_t ds18b20_get_temperature(const ds18b20_handle_t *const handle, float *const temperature) {
+    if(handle == NULL)
+        return DS18B20_ERROR_INVALID_HANDLE;
+    
+    if(temperature == NULL)
+        return DS18B20_ERROR_INVALID_ARGUMENT;
+
+    const uint8_t mask = 0xff << (3 - (handle->scratchpad[4] >> 5));
+    *temperature = ((handle->scratchpad[1] << 8) | (handle->scratchpad[0] & mask)) / 16.0f;
+
+    return DS18B20_OK;
+}
+
+ds18b20_error_t ds18b20_get_configuration(const ds18b20_handle_t *const handle, ds18b20_config_t *const config) {
+    if(handle == NULL)
+        return DS18B20_ERROR_INVALID_HANDLE;
+    
+    if(config == NULL)
+        return DS18B20_ERROR_INVALID_CONFIG;
+
+    config->trigger_high = handle->scratchpad[2];
+    config->trigger_low = handle->scratchpad[3];
+    config->resolution = handle->scratchpad[4];
+
+    return DS18B20_OK;
 }
--- src/ds18b20.h
+++ src/ds18b20.h
@@ -30,12 +30,22 @@
 #define DS18B20_ROM_CODE_SIZE 8
 #define DS18B20_SCRATCHPAD_SIZE 9
 
+
+
 typedef enum {
     DS18B20_RESOLUTION_9_BIT = 0b00011111,
     DS18B20_RESOLUTION_10_BIT = 0b00111111,
     DS18B20_RESOLUTION_11_BIT = 0b01011111,
     DS18B20_RESOLUTION_12_BIT = 0b01111111
 } ds18b20_resolution_t;
+
+typedef enum {
+    DS18B20_ERROR_UNKNOWN = -1,
+    DS18B20_OK = 0,
+    DS18B20_ERROR_INVALID_HANDLE,
+    DS18B20_ERROR_INVALID_CONFIG,
+    DS18B20_ERROR_INVALID_ARGUMENT
+} ds18b20_error_t;
 
 typedef struct {
     int8_t trigger_high;
@@ -50,20 +60,20 @@ typedef struct {
     uint8_t scratchpad[DS18B20_SCRATCHPAD_SIZE];
 } ds18b20_handle_t;
 
-void ds18b20_reset(const ds18b20_handle_t *const handle);
+ds18b20_error_t ds18b20_reset(const ds18b20_handle_t *const handle);
 
-void ds18b20_send_command(const ds18b20_handle_t *const handle, const uint8_t command);
+ds18b20_error_t ds18b20_send_command(const ds18b20_handle_t *const handle, const uint8_t command);
 
-void ds18b20_read_rom(ds18b20_handle_t *const handle);
+ds18b20_error_t ds18b20_read_rom(ds18b20_handle_t *const handle);
 
-void ds18b20_write_scratchpad(const ds18b20_handle_t *const handle, const uint8_t *const data);
-void ds18b20_read_scratchpad(ds18b20_handle_t *const handle, const uint8_t num_of_bytes_to_read);
+ds18b20_error_t ds18b20_write_scratchpad(const ds18b20_handle_t *const handle, const uint8_t *const data);
+ds18b20_error_t ds18b20_read_scratchpad(ds18b20_handle_t *const handle, const uint8_t num_of_bytes_to_read);
 
-void ds18b20_init(ds18b20_handle_t *const handle, const ds18b20_config_t *const config);
-void ds18b20_configure(ds18b20_handle_t *const handle, const ds18b20_config_t *const config);
-void ds18b20_read_temperature(ds18b20_handle_t *const handle, float *const temperature);
+ds18b20_error_t ds18b20_init(ds18b20_handle_t *const handle, const ds18b20_config_t *const config);
+ds18b20_error_t ds18b20_configure(ds18b20_handle_t *const handle, const ds18b20_config_t *const config);
+ds18b20_error_t ds18b20_read_temperature(ds18b20_handle_t *const handle, float *const temperature);
 
-void ds18b20_get_temperature(const ds18b20_handle_t *const handle, float *const temperature);
-void ds18b20_get_configuration(const ds18b20_handle_t *const handle, ds18b20_config_t *const config);
+ds18b20_error_t ds18b20_get_temperature(const ds18b20_handle_t *const handle, float *const temperature);
+ds18b20_error_t ds18b20_get_configuration(const ds18b20_handle_t *const handle, ds18b20_config_t *const config);
 
 #endif
