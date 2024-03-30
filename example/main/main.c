31d1b5b413c33ad5ea74a86c1e5bb9e3acf948a3
filizw <fizworski202@gmail.com>
2024-03-30 14:06:26 +0100

    Simplify error handling and delete some redundant code

--- example/main/main.c
+++ example/main/main.c
@@ -19,20 +19,17 @@ void read_temperature(void *arg) {
 
 void app_main(void) {
     ds18b20_config_t config = {
-        .gpio_pin = DS18B20_PIN,
+        .owb_pin = DS18B20_PIN,
         .enable_crc = DS18B20_CRC_ENABLE,
         .trigger_high = DS18B20_MAX_TEMPERATURE,
         .trigger_low = DS18B20_MIN_TEMPERATURE,
         .resolution = CONVERSION_RESOLUTION
     };
 
-    if(!ds18b20_error_check(ds18b20_init(&handle, &config), "main", NULL)) {
-        ds18b20_get_configuration(&handle, &config);
-
-        ESP_LOGI("rom_code", "family code: %d", handle.rom_code[0]);
-        ESP_LOGI("config", "trigger high: %d, trigger low: %d, resolution: %d", config.trigger_high, config.trigger_low, config.resolution);
-        ESP_LOGI("scratchpad CRC", "%d", handle.scratchpad[DS18B20_SCRATCHPAD_SIZE - 1]);
-
+    if(!ds18b20_init(&handle, &config)) {
         xTaskCreate(read_temperature, "temp", 4096, NULL, 10, NULL);
+    }
+    else {
+        ESP_LOGE("main", "ds18b20 init failed");
     }
 }
